6b3029cf9ca12e640426d90b6ff74db099c52065
jochoaserna <jochoaserna@hotmail.com>
2015-05-17 20:18:40 +0200

    Autenticación y sesión

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 
@@ -22,10 +23,18 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  if (!req.path.match(/\/login|\/logout/)) {   // guardar path en session.redir para despues de login
+    req.session.redir = req.path;
+  }
+  res.locals.session = req.session;   // Hacer visible req.session en las vistas
+  next();
+});
 app.use('/', routes);
 
 // catch 404 and forward to error handler
